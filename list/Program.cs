bc72a5a4e527ff313ed0c58f3954f303c9da25c0
maxaiette1129 <maxaiettec@gmail.com>
2023-05-07 21:25:05 +0800

    Add project files.

---
+++ list/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace list
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+           List<int> nums = new List<int>(); 
+
+           nums.Add(05);
+           nums.Add(20);
+           nums.Add(30);
+           nums.Add(50);
+           nums.Add(100);
+
+            int[] numsToArray = nums.ToArray();
+
+            for (int i = 0; i < numsToArray.Length; i++)
+            {
+                Console.WriteLine(numsToArray[i]);
+            }
+    
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+        }
+    }
+}
